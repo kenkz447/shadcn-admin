dbfdc9911fe617ac8726e2da26ec2d1b1de01968
kenkz447 <kenkz447@gmail.com>
2026-03-24 11:22:46 +0700

    feat: update button styles and add CSS variables for theming

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,45 +9,43 @@ import { Slot } from 'radix-ui'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  "inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "inline-flex shrink-0 items-center justify-center gap-2 text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       color: {
-        secondary: "[--btn-bg:var(--secondary)] [--btn-fg:var(--secondary-foreground)] [--btn-border:var(--secondary)]",
-        primary: "[--btn-bg:var(--primary)] [--btn-fg:var(--primary-foreground)] [--btn-border:var(--primary)]",
-        destructive: "[--btn-bg:var(--destructive)] [--btn-fg:var(--destructive-foreground)] [--btn-border:var(--destructive)]",
+        primary: "[--btn-bg:var(--color-btn-primary)] [--btn-fg:var(--color-btn-primary-foreground)] [--btn-border:var(--color-btn-primary)] [--btn-bg-hover:var(--color-btn-primary)/80] [--btn-fg-hover:var(--color-btn-primary-foreground)/80] [--btn-border-hover:var(--color-btn-primary)/80]",
+        secondary: "[--btn-bg:var(--color-btn-secondary)] [--btn-fg:var(--color-btn-secondary-foreground)] [--btn-border:var(--color-btn-secondary)] [--btn-bg-hover:var(--color-btn-secondary)] [--btn-fg-hover:var(--color-btn-secondary-foreground)/80] [--btn-border-hover:var(--color-btn-secondary)/80]",
+        destructive: "[--btn-bg:var(--color-btn-destructive)] [--btn-fg:var(--color-btn-destructive-foreground)] [--btn-border:var(--color-btn-destructive)] [--btn-bg-hover:var(--color-btn-destructive)/80] [--btn-fg-hover:var(--color-btn-destructive-foreground)/80] [--btn-border-hover:var(--color-btn-destructive)/80]",
       },
       appearance: {
-        solid: "bg-[var(--btn-bg)] text-[var(--btn-fg)] border-[var(--btn-border)] hover:bg-[var(--btn-bg)/90] focus-visible:ring-[var(--btn-border)]/20 dark:focus-visible:ring-[var(--btn-border)]/40",
-        outline: "border-[var(--btn-border)] bg-transparent shadow-xs hover:bg-[var(--btn-bg)/10] hover:text-[var(--btn-fg)]",
-        ghost: "hover:bg-[var(--btn-bg)/10] hover:text-[var(--btn-fg)]",
+        solid: "bg-[var(--btn-bg)] text-[var(--btn-fg)] hover:bg-(--btn-bg-hover) hover:text-[var(--btn-fg-hover)]",
+        outline: "bg-transparent border-[var(--btn-border)] hover:bg-[var(--btn-bg-hover)] hover:text-[var(--btn-fg-hover)] hover:border-[var(--btn-border-hover)]",
+        ghost: "hover:bg-[var(--btn-bg)] hover:text-[var(--btn-fg)]",
         link: "text-[var(--btn-bg)] underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        xs: "h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: "h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
-        "icon-xs": "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
-        "icon-sm": "size-8",
-        "icon-lg": "size-10",
+        md: "h-btn-md px-btn-px-md rounded-btn-md",
+        sm: "h-btn-sm px-btn-px-sm rounded-btn-sm",
+        lg: "h-btn-lg px-btn-px-lg rounded-btn-lg",
+        icon: "size-btn-md rounded-btn-md",
+        "icon-sm": "size-btn-sm rounded-btn-sm",
+        "icon-lg": "size-btn-lg rounded-btn-lg",
       },
     },
     defaultVariants: {
       color: "primary",
       appearance: "solid",
-      size: "default",
+      size: "md",
     },
   }
 )
 
 function Button({
   className,
-  appearance = "solid",
-  size = "default",
-  color = "primary",
-  asChild = false,
+  appearance,
+  size,
+  color,
+  asChild,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
