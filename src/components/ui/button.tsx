112dc1bc1a7d651642bcc390dbad2a55b98f4430
kenkz447 <kenkz447@gmail.com>
2026-03-25 13:49:34 +0700

    refactor: update button styles and variable names for consistency

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,27 +9,27 @@ import { Slot } from 'radix-ui'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  "focus-visible:border-ring focus-visible:ring-(--button-ring-color-focus) aria-invalid:ring-(--button-ring-color-invalid) aria-invalid:border-(--button-border-color-invalid) rounded-(--button-radius) border border-transparent bg-clip-padding text-(length:--button-text-size) font-medium focus-visible:ring-(length:--button-ring-width-focus) aria-invalid:ring-(length:--button-ring-width-invalid) active:translate-y-px [&_svg:not([class*='size-'])]:size-(--button-icon-size) group/button inline-flex shrink-0 items-center justify-center whitespace-nowrap transition-all outline-none select-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+  "focus-visible:border-ring focus-visible:ring-(--btn-ring-color-focus) aria-invalid:ring-(--btn-ring-color-invalid) aria-invalid:border-(--btn-border-color-invalid) rounded-(--btn-radius) border border-transparent bg-clip-padding text-(length:--btn-text-size) font-medium focus-visible:ring-(length:--btn-ring-width-focus) aria-invalid:ring-(length:--btn-ring-width-invalid) active:translate-y-px [&_svg:not([class*='size-'])]:size-(--btn-icon-size) group/button inline-flex shrink-0 items-center justify-center whitespace-nowrap transition-all outline-none select-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
   {
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground [a]:hover:bg-primary/80",
-        outline: "border-(--button-outline-border-color) bg-(--button-outline-bg) hover:bg-(--button-outline-bg-hover) hover:text-foreground aria-expanded:bg-(--button-outline-bg-hover) aria-expanded:text-foreground",
+        outline: "border-(--btn-outline-border-color) bg-(--btn-outline-bg) hover:bg-(--btn-outline-bg-hover) hover:text-foreground aria-expanded:bg-(--btn-outline-bg-hover) aria-expanded:text-foreground",
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80 aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
-        ghost: "hover:bg-(--button-ghost-bg-hover) hover:text-foreground aria-expanded:bg-(--button-ghost-bg-hover) aria-expanded:text-foreground",
-        destructive: "bg-(--button-destructive-bg) hover:bg-(--button-destructive-bg-hover) focus-visible:ring-(--button-destructive-ring-color-focus) text-destructive focus-visible:border-(--button-destructive-border-color-focus)",
+        ghost: "hover:bg-(--btn-ghost-bg-hover) hover:text-foreground aria-expanded:bg-(--btn-ghost-bg-hover) aria-expanded:text-foreground",
+        destructive: "bg-(--btn-destructive-bg) hover:bg-(--btn-destructive-bg-hover) focus-visible:ring-(--btn-destructive-ring-color-focus) text-destructive focus-visible:border-(--btn-destructive-border-color-focus)",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default:
-          "h-(--button-height) gap-(--button-gap) px-(--button-padding-x) has-data-[icon=inline-end]:pr-(--button-padding-inline-end) has-data-[icon=inline-start]:pl-(--button-padding-inline-start)",
-        xs: "h-(--button-height-xs) gap-(--button-gap-xs) rounded-(--button-radius-xs) px-(--button-padding-x-xs) text-(length:--button-text-size-xs) in-data-[slot=button-group]:rounded-(--button-group-radius) has-data-[icon=inline-end]:pr-(--button-padding-inline-end-xs) has-data-[icon=inline-start]:pl-(--button-padding-inline-start-xs) [&_svg:not([class*='size-'])]:size-(--button-icon-size-xs)",
-        sm: "h-(--button-height-sm) gap-(--button-gap-sm) rounded-(--button-radius-sm) px-(--button-padding-x-sm) text-(length:--button-text-size-sm) in-data-[slot=button-group]:rounded-(--button-group-radius) has-data-[icon=inline-end]:pr-(--button-padding-inline-end-sm) has-data-[icon=inline-start]:pl-(--button-padding-inline-start-sm) [&_svg:not([class*='size-'])]:size-(--button-icon-size-sm)",
-        lg: "h-(--button-height-lg) gap-(--button-gap-lg) px-(--button-padding-x-lg) has-data-[icon=inline-end]:pr-(--button-padding-inline-end-lg) has-data-[icon=inline-start]:pl-(--button-padding-inline-start-lg)",
-        icon: "size-(--button-size-icon)",
-        "icon-xs": "size-(--button-size-icon-xs) rounded-(--button-radius-xs) in-data-[slot=button-group]:rounded-(--button-group-radius) [&_svg:not([class*='size-'])]:size-(--button-icon-size-xs)",
-        "icon-sm": "size-(--button-size-icon-sm) rounded-(--button-radius-sm) in-data-[slot=button-group]:rounded-(--button-group-radius)",
-        "icon-lg": "size-(--button-size-icon-lg)",
+          "h-(--btn-height) gap-(--btn-gap) px-(--btn-padding-x) has-data-[icon=inline-end]:pr-(--btn-has-icon-padding) has-data-[icon=inline-start]:pl-(--btn-has-icon-padding)",
+        xs: "h-(--btn-height-xs) gap-(--btn-gap-xs) rounded-(--btn-radius-xs) px-(--btn-padding-x-xs) text-(length:--btn-text-size-xs) in-data-[slot=button-group]:rounded-(--btn-group-radius) has-data-[icon=inline-end]:pr-(--btn-has-icon-padding-xs) has-data-[icon=inline-start]:pl-(--btn-has-icon-padding-xs) [&_svg:not([class*='size-'])]:size-(--btn-icon-size-xs)",
+        sm: "h-(--btn-height-sm) gap-(--btn-gap-sm) rounded-(--btn-radius-sm) px-(--btn-padding-x-sm) text-(length:--btn-text-size-sm) in-data-[slot=button-group]:rounded-(--btn-group-radius) has-data-[icon=inline-end]:pr-(--btn-has-icon-padding-sm) has-data-[icon=inline-start]:pl-(--btn-has-icon-padding-sm) [&_svg:not([class*='size-'])]:size-(--btn-icon-size-sm)",
+        lg: "h-(--btn-height-lg) gap-(--btn-gap-lg) px-(--btn-padding-x-lg) has-data-[icon=inline-end]:pr-(--btn-has-icon-padding-lg) has-data-[icon=inline-start]:pl-(--btn-has-icon-padding-lg)",
+        icon: "size-(--btn-size-icon)",
+        "icon-xs": "size-(--btn-size-icon-xs) rounded-(--btn-radius-xs) in-data-[slot=button-group]:rounded-(--btn-group-radius) [&_svg:not([class*='size-'])]:size-(--btn-icon-size-xs)",
+        "icon-sm": "size-(--btn-size-icon-sm) rounded-(--btn-radius-sm) in-data-[slot=button-group]:rounded-(--btn-group-radius)",
+        "icon-lg": "size-(--btn-size-icon-lg)",
       },
     },
     defaultVariants: {
